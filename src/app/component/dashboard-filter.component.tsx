32206c9ca6c78ea29f0387da4e58a83da23e49e1
Arthur Gevaert <130151075+Arthur-Gev2023@users.noreply.github.com>
2023-04-27 13:59:31 +0200

    Ajout de commentaires dans Filter-Dashboard.component
    Ajout de commentaire sur la fonction DashboardFilter

--- src/app/component/dashboard-filter.component.tsx
+++ src/app/component/dashboard-filter.component.tsx
@@ -6,36 +6,44 @@ import { ProjectResponse } from '../types/project.response';
 
 
 function DashboardFilter() {
+  // Utilisation du Hook useState pour stocker l'état des projets récupérés depuis l'API
   const [projects, setProjects] = useState<ProjectResponse[]>([]);
+  // Utilisation du Hook useState pour stocker l'ID du projet sélectionné dans le menu déroulant
   const [selectedProjectId, setSelectedProjectId] = useState<number>();
+  // Utilisation du Hook useState pour stocker les requêtes associées au projet sélectionné
   const [requests, setRequests] = useState<{ id: number; name: string }[]>([]);
+  // Utilisation du Hook useState pour stocker l'ID de la requête sélectionnée dans le menu déroulant
   const [selectedRequestId , setSelectedRequestId]= useState<number>()
 
+  // Utilisation du Hook useEffect pour effectuer l'appel à l'API lors du montage du composant
   useEffect(() => {
+    // Définition d'une fonction asynchrone pour appeler l'API
     const fetchData = async () => {
       try {
+        // Appel de la fonction callApiToGetAllProjects qui retourne un Observable
         const subscription = callApiToGetAllProjects().subscribe({
+          // Callback exécuté lorsque des données sont reçues
           next: (projects) => {
-            // Handle the response data here
- 
+            // Mise à jour de l'état des projets
             setProjects(projects);
-            // mettre dans la liste déroulate des requests la liste des requests contenue dans ton 1er project
+            // Si des projets ont été reçus, on sélectionne le premier projet et on met à jour la liste des requêtes associées
             if(projects && projects.length > 0) {
               const firstProject: ProjectResponse = projects[0];
               setSelectedProjectId(firstProject.id)
               setRequests(firstProject.requests)
             }
           },
+          // Callback exécuté en cas d'erreur
           error: (err) => {
-            // Handle any errors that occur during the API call
             console.error(err);
           },
+          // Callback exécuté lorsque l'Observable est terminé
           complete: () => {
-            // Handle any completion logic here
             console.log('API call completed successfully');
           },
         });
 
+        // Retour d'une fonction de nettoyage pour annuler la souscription à l'Observable
         return () => subscription.unsubscribe();
       } catch (error) {
         console.log(error);
@@ -43,24 +51,29 @@ function DashboardFilter() {
       }
     };
 
+    // Appel de la fonction fetchData
     fetchData();
   }, []);
 
-  
-
+  // Fonction exécutée lorsque la sélection du projet change
   const handleProjectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    // Récupération de l'ID du projet sélectionné
     const projectId: number = Number(event.target.value);
+    // Mise à jour de l'ID du projet sélectionné
     setSelectedProjectId(projectId);
+    // Récupération du projet sélectionné à partir de la liste des projets
     const selectedProject: ProjectResponse | undefined = projects.find(project => project.id === projectId );
+    // Si un projet est sélectionné, on met à jour la liste des requêtes associées
     if(selectedProject)
-      setRequests(selectedProject?.requests)
+      setRequests(selectedProject.requests)
+      // Sinon, on vide la liste des requêtes
       else
       setRequests([]);
   };
 
-  
+  // Fonction exécutée lorsque la sélection de la requête change
   const handleRequestChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    // Do something with the selected request ID
+    // Mise à jour de l'ID de la requête sélectionnée
     setSelectedRequestId(Number(event.target.value));
     console.log('Selected request ID:', event.target.value);
   };
